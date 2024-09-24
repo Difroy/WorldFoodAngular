d5378a7ce317880d2235fd6c025838e860ce6ef6
Difroy <unifroy@gmail.com>
2024-09-24 17:54:09 +0200

    Update worker.component.ts

--- src/app/worker/worker.component.ts
+++ src/app/worker/worker.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Worker } from '../model/Worker';
+import { NgIf } from '@angular/common';
 
-@Component({
+
+@Component
+({
   selector: 'app-worker',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './worker.component.html',
   styleUrl: './worker.component.css'
 })
 export class WorkerComponent {
+
+
+
+  @Input()
+  worker!:Worker
+
+  fullview:boolean = false;
+
+  toggleDetail():void
+  {
+
+    this.fullview = !this.fullview;
+
+  }
+  
+
+
+
+
+
 
 }
