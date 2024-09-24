8d6b2dcf37907c97a5a48ddafa5aa46b37bff0c1
Difroy <unifroy@gmail.com>
2024-09-24 15:21:33 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,16 +11,3 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient()  // Aggiungi il nuovo provider HttpClient qui
   ]
 };
-
-/*
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
-
-
-*/
